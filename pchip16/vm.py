7305603a7c94d1ed6456380294780517e33dc2b8
Tim Brooks <brooks@cern.ch>
2013-04-15 03:30:46 +0100

    Added first JMP opcode

--- pchip16/vm.py
+++ pchip16/vm.py
@@ -11,3 +11,13 @@ class VM(object):
     def step(self):
         """Execute instruction at self.program_counter and increment"""
         self.program_counter += 1
+    def execute(self, op_code):
+        """Carry out instruction sepcified by op_code"""
+        if op_code >> 28 == 0x1:
+            #"""Jumps"""
+            if op_code >> 16 == 0x1000:
+                #"""JMP HHLL"""
+                hh_addr = op_code - (op_code >> 8 << 8)
+                ll_addr = (op_code - hh_addr - (op_code >> 16 << 16) ) >> 8
+
+                self.program_counter = (hh_addr << 8) + ll_addr
--- pchip16_tests.py
+++ pchip16_tests.py
@@ -13,3 +13,12 @@ class TestVM(unittest.TestCase):
         self.assertEqual(0, vmac.program_counter)
         vmac.step()
         self.assertEqual(1, vmac.program_counter)
+    def test_nop_instruction(self):
+        vmac = VM()
+        vmac.execute(0x00000000)
+    def test_JMP_HHLL_instruction(self):
+        vmac = VM()
+        vmac.execute(0x10000000)
+        self.assertEqual(0, vmac.program_counter)
+        vmac.execute(0x10003412)
+        self.assertEqual(0x1234, vmac.program_counter)
