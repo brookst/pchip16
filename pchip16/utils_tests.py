d74490b1e905b3c8eaa0db3f13cf2f7a8789034f
Tim Brooks <brooks@cern.ch>
2013-04-17 02:04:15 +0100

    Added 0th twos compliment test

--- pchip16/utils_tests.py
+++ pchip16/utils_tests.py
@@ -8,6 +8,10 @@ import pchip16.utils as utils
 
 class TestTwosCompliment(unittest.TestCase):
     """Test twos-compliment helper functions"""
+    def test_0_to_dec(self):
+        self.assertEqual(utils.to_dec(0x0), 0)
+    def test_0_to_hex(self):
+        self.assertEqual(utils.to_hex(0), 0x0)
     def test_1_to_dec(self):
         self.assertEqual(utils.to_dec(0x1), 1)
     def test_1_to_hex(self):
@@ -16,11 +20,11 @@ class TestTwosCompliment(unittest.TestCase):
         self.assertEqual(utils.to_dec(0x7FFF), 32767)
     def test_max_to_hex(self):
         self.assertEqual(utils.to_hex(32767), 0x7FFF)
-    def test_neg_max_to_dec(self):
-        self.assertEqual(utils.to_dec(0x8000), -32768)
-    def test_neg_max_to_hex(self):
-        self.assertEqual(utils.to_hex(-32768), 0x8000)
     def test_neg_one_to_dec(self):
         self.assertEqual(utils.to_dec(0xFFFF), -1)
     def test_neg_one_to_hex(self):
         self.assertEqual(utils.to_hex(-1), 0xFFFF)
+    def test_neg_max_to_dec(self):
+        self.assertEqual(utils.to_dec(0x8000), -32768)
+    def test_neg_max_to_hex(self):
+        self.assertEqual(utils.to_hex(-32768), 0x8000)
