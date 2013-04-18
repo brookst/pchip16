4c18f7fd7c843f12f8aedd06791990fc7444a0dd
Tim Brooks <brooks@cern.ch>
2013-04-18 02:25:21 +0100

    Added classifiers and README into setup

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 """
 pchip16 setup routine
 """
-#pylint: disable=W0611
+#pylint: disable=I0011,W0611
 
 from setuptools import setup
+# Prevent issue with setup.py test
 try:
     import multiprocessing
 except ImportError:
@@ -12,6 +13,11 @@ except ImportError:
 setup(name='pchip16',
       version='0.1',
       description='Python chip16 virtual machine',
+      long_description=open('README.rst').read(),
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Education',
+      ],
       url='http://github.com/brookst/pchip16',
       author='Tim Brooks',
       author_email='brooks@skoorb.net',
