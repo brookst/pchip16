e3387789128b88159ee39b8ff853363a7c12b6df
Tim Brooks <brooks@cern.ch>
2013-05-06 18:49:55 +0100

    Made sprite move under control of arrows

--- pygame_dev.py
+++ pygame_dev.py
@@ -4,18 +4,27 @@ pchip16 interface - graphics audio and controls module
 
 import pygame
 
+DIRMAP = {
+    273:(0, -1), #Up
+    274:(0, 1),  #Down
+    275:(1, 0),  #Right
+    276:(-1, 0), #Left
+}
+
 def nibble_iter(data):
     """Yield back data, one nibble at a time"""
     while data:
         yield data & 0xF
         data = data >> 4
 
-# pylint: disable-msg=R0903
+# pylint: disable-msg=I0011,R0903
 class Sprite(pygame.sprite.Sprite):
     """Class for a contiguous set of pixel values"""
     def __init__(self, data, width, height, postition):
         self.position = postition
+        self.velocity = 0, 1
         pygame.sprite.Sprite.__init__(self)
+        # pylint: disable-msg=I0011,E1121
         self.image = pygame.Surface([width, height])
         for i, nibble in enumerate(nibble_iter(data) ):
             if nibble:
@@ -30,6 +39,7 @@ class Interface:
         self._display_surf = None
         self.clock = pygame.time.Clock()
         self.size = 320, 240
+        self.sprite = Sprite(0xFFFFF00FF00FFFFF, 4, 4, (0, 0) )
  
     def init(self):
         """Create display"""
@@ -42,13 +52,16 @@ class Interface:
         """Handle input"""
         if event.type == pygame.QUIT:
             self._running = False
+        elif event.type == pygame.KEYDOWN and 272 < event.key < 277:
+            self.sprite.velocity = DIRMAP[event.key]
 
     def loop(self):
         """Interpret commands"""
-        self._display_surf.fill( (0, 0, 0) )
-        position = self.frame % 316, self.frame % 236
-        sprite = Sprite(0xFFFFF00FF00FFFFF, 4, 4, position)
-        self._display_surf.blit(sprite.image, sprite.position)
+        #self._display_surf.fill( (0, 0, 0) )
+        d_x, d_y = self.sprite.position
+        v_x, v_y = self.sprite.velocity
+        self.sprite.position = (d_x + v_x) % 320, (d_y + v_y) % 240
+        self._display_surf.blit(self.sprite.image, self.sprite.position)
         self.frame += 1
 
     def render(self):
